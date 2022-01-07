994c3ee837f63ecccca69923a0968d9e6fe27ef6
makamys <makamys@outlook.com>
2022-01-07 01:53:25 +0100

    Init project

--- src/main/java/api/player/Renderplayerapi.java
+++ src/main/java/api/player/Renderplayerapi.java
@@ -1,14 +1,14 @@
-package ${package};
+package api.player;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ${mainclass}.MODID, version = ${mainclass}.VERSION)
-public class ${mainclass}
+@Mod(modid = Renderplayerapi.MODID, version = Renderplayerapi.VERSION)
+public class Renderplayerapi
 {
-    public static final String MODID = "${modid}";
+    public static final String MODID = "RenderPlayerAPI";
     public static final String VERSION = "@VERSION@";
 
     @EventHandler
